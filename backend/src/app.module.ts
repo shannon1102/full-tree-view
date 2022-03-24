2a3726e66db7fc45d5c327d7f1622d60d2134665
QuangHuyy <50898345+QuangHuyy@users.noreply.github.com>
2022-03-24 09:46:34 +0700

    khử module con

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
+import { CatsService } from './cats/cats.service';
+import { Cat, CatSchema } from './cats/schemas/cat.schema';
+import { DepartmentsController } from './departments/departments.controller';
 import { DepartmentsModule } from './departments/departments.module';
+import { DepartmentsService } from './departments/departments.service';
+import { Department, DepartmentSchema } from './departments/schemas/department.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://Hwuang:q@cluster0.ot4kn.mongodb.net/departments_management?retryWrites=true&w=majority'),
-    CatsModule,
-    DepartmentsModule,
+    MongooseModule.forFeature([{ name: Department.name, schema: DepartmentSchema }]),
+    MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
+    // CatsModule,
+    // DepartmentsModule,
   ],
+  controllers: [
+    DepartmentsController,
+    CatsController,
+  ],
+  providers: [
+    DepartmentsService,
+    CatsService
+  ]
 })
-export class AppModule {}
+export class AppModule { }
